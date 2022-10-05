3978ddccb205d4622ffe6fc7cf9fcbf2fa798318
TheWojwymX <115039019+TheWojwymX@users.noreply.github.com>
2022-10-05 13:13:48 +0200

    Functions

--- plot_main.py
+++ plot_main.py
@@ -1,4 +1,4 @@
-
+import csv
 import pandas as pd
 import scripts as scr
 df = pd.read_csv('data.csv',
@@ -8,10 +8,23 @@ print(df)
 df['Gatunek'].replace([0, 1, 2], ["setosa", "versicolor", "virginica"], inplace=True)
 print(df)
 
-df_species_counts = df.Gatunek.value_counts().reset_index()
-df_species_counts.columns = ["Gatunek", "Liczebność"]
+#df_species_counts = df.Gatunek.value_counts().reset_index()
+#df_species_counts.columns = ["Gatunek", "Liczebność"]
 
-scr.multiple_histogram(df, '1', "Długość działki kielicha [cm]", "multi_histogram1.pdf")
-scr.multiple_histogram(df, '2', "Szerokość działki kielicha [cm]", "multi_histogram2.pdf")
-scr.multiple_histogram(df, '3', "Długość płatka [cm]", "multi_histogram3.pdf")
-scr.multiple_histogram(df, '4', "Szerokość płatka [cm]", "multi_histogram4.pdf")
+#scr.multiple_histogram(df, '1', "Długość działki kielicha [cm]", "multi_histogram1.pdf")
+#scr.multiple_histogram(df, '2', "Szerokość działki kielicha [cm]", "multi_histogram2.pdf")
+#scr.multiple_histogram(df, '3', "Długość płatka [cm]", "multi_histogram3.pdf")
+#scr.multiple_histogram(df, '4', "Szerokość płatka [cm]", "multi_histogram4.pdf")
+
+list = []
+with open('data.csv', 'r') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    for row in csv_reader:
+        list.append(row[0])
+final = [float(i) for i in list]
+
+print("max: ", scr.maximum(final))
+print("min: ", scr.minimum(final))
+print("sre: ", scr.mean(final, len(final)))
+print("med: ", scr.median(final))
+print("deriv: ", scr.deriv(final))
--- scripts.py
+++ scripts.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-
+import math
 
 def multiple_histogram(datafile, column, axis_label,output_name):
     dl = datafile[column].loc[datafile.Gatunek == "setosa"]
@@ -17,3 +17,39 @@ def multiple_histogram(datafile, column, axis_label,output_name):
     plt.legend()
     plt.savefig(output_name)
     plt.clf()
+
+def mean(a, n):
+    sum = 0
+    for i in range(0, n):
+        sum = sum + a[i]
+    return float(sum / n)
+
+def median(a):
+    a.sort()
+    if len(a) % 2 != 0:
+        temp1 = int((len(a) + 1) / 2 - 1)
+        return a[temp1]
+    else:
+        temp1 = int(len(a) / 2 - 1)
+        temp2 = int(len(a) / 2)
+        return (a[temp1] + a[temp2]) / 2
+
+def deriv(a):
+    sum = 0
+    for i in range(len(a)):
+        sum = sum + (a[i] - mean(a, len(a))) * (a[i] - mean(a, len(a)))
+    return float(math.sqrt(sum / len(a)))
+
+def maximum(a):
+    temp = a[0]
+    for x in a:
+        if x > temp:
+            temp = x
+    return temp
+
+def minimum(a):
+    temp = a[0]
+    for x in a:
+        if x < temp:
+            temp = x
+    return temp
